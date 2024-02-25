335aa26e5f5c15eabd27a8fb2732c5af75406d68
AryaJagtap <156169733+AryaJagtap@users.noreply.github.com>
2024-02-25 18:52:49 +0000

    updated

--- 01_Basics/01_variables.js
+++ 01_Basics/01_variables.js
@@ -2,17 +2,16 @@ const accountId = 144553
 let accountEmail = "arya@google.com" // let is used inside the scope , scope means curly brackets {}
 var accountPassword = "12345" // change in variables changes every variable in the code , it does not follows curly brakets{} (daso jage change ho jayega agar ek jage change kiya to.)
 accountCity = "Mumbai" // variable can be declared in this way also, but this metho of decleration is not recommended.
-let AccountState,
+let AccountState;   // id such decleration is made then it gives output as undefined.
 
 // accountId = 2   // const cannot be changed
 
- // accountEmail = "ab@kfnidsnfv"
-//accountPassword = 12221212
-//accountCity = "Bengaluru"
+accountEmail = "ab@kfnidsnfv"
+accountPassword = 12221212
+accountCity = "Bengaluru"
 
 
-
-// console.table([accountId,accountEmail,accountPassword,accountCity,AccountState])
+console.table([accountId,accountEmail,accountPassword,accountCity,AccountState])
 
 // this is a single line comment
 /* this is a multi line omment*/
@@ -20,12 +19,3 @@ let AccountState,
 /* prefer not to use var
 because of issue in block scope and functional scope
 */
-
-
-
-
-
-let apple = "fruit"
-apple = "vegetable"
-
-console.log(apple)
