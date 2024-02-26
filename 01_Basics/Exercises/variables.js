f1605b60ac420716c4dd5f9307909db37085c189
AryaJagtap <156169733+AryaJagtap@users.noreply.github.com>
2024-02-26 15:49:32 +0000

    started exercise on variables  from https://www.teaching-materials.org/javascript/exercises/variables

---
+++ 01_Basics/Exercises/variables.js
@@ -0,0 +1,16 @@
+//Exercises: Variables
+
+/*    (1)The Fortune Teller
+Why pay a fortune teller when you can just program your fortune yourself?
+
+Store the following into variables: number of children, partner's name, geographic location, job title.
+Output your fortune to the screen like so: "You will be a X in Y, and married to Z with N kids." */
+
+let children = 4
+let wife = "queen"
+let location = "whole world"
+let jobtitle = "king"
+
+let future = "you will be a " + jobtitle + " in " + location + " , and married to " + wife + " with " + children + " kids. "
+
+console.log(future);
