350a539ec9673b5a397a9e7b5572c8dd1f97cd37
AryaJagtap <156169733+AryaJagtap@users.noreply.github.com>
2024-03-06 20:15:06 +0000

    learned comparisons

---
+++ 01_Basics/04_comparisons.js
@@ -0,0 +1,40 @@
+// > - greater than
+// < - smaller than
+// == - equality check
+// <= - smaller than equal to
+// >= - greater than equal to 
+// != - not equal
+// === strict check 
+console.log("2" = 2);
+console.log(2 = 2);
+
+
+// 1. BETWEEN STRINGS
+console.log("2" > 1);
+console.log("02" > 1);
+
+// 2. BETWEEN NUMBERS
+console.log(2 > 1);
+console.log(2 >= 1);
+console.log(2 = 1);
+console.log(2 <= 1);
+console.log(2 == 1);
+console.log(2 != 1);
+console.log(2 < 1);
+
+// 3. BETWEEN NULL 
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+// 4. BETWEEN UNDEFINED 
+console.log(undefined = 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// The reason is that an empty check == and comparison > < >= =< work differently
+// comparisos convert null to a number , treating it as 0.
+// thats why (3) null >= 0 is true and (1) null >0 is false.
+
+
+
