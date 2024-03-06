a8f0d8a70f356f82e3cbe63041a609856468b45a
AryaJagtap <156169733+AryaJagtap@users.noreply.github.com>
2024-03-06 14:20:22 +0000

    Changes to be committed: 	modified:   01_Basics/01_variables.js 	renamed:    01_Basics/Exercises/variables.js -> 01_Basics/Exercises/01_variables.js 	new file:   01_Basics/Exercises/02_datatypes

--- 01_Basics/01_variables.js
+++ 01_Basics/01_variables.js
@@ -1,7 +1,7 @@
 const accountId = 144553
 let accountEmail = "arya@google.com" // let is used inside the scope , scope means curly brackets {}
 var accountPassword = "12345" // change in variables changes every variable in the code , it does not follows curly brakets{} (daso jage change ho jayega agar ek jage change kiya to.)
-accountCity = "Mumbai" // variable can be declared in this way also, but this metho of decleration is not recommended.
+accountCity = "Mumbai" // variable can be declared in this way also, but this method of decleration is not recommended.
 let AccountState;   // id such decleration is made then it gives output as undefined.
 
 // accountId = 2   // const cannot be changed
