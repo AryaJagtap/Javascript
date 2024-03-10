4ec9c6be7b4e200cf08cae3736dc6c26993282c4
AryaJagtap <156169733+AryaJagtap@users.noreply.github.com>
2024-03-10 16:33:41 +0000

    started basics 2 with arrays

---
+++ 02_Basics/01_arrays.js
@@ -0,0 +1,9 @@
+// Arrays
+
+// () this are parenthesies
+// [] this are brackets
+// {} this are braces
+
+// link for study 
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
+
