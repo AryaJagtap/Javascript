62aa2a9d275ba279f0a16518a40029398dd04312
Arya Jagtap <156169733+AryaJagtap@users.noreply.github.com>
2024-03-13 08:04:30 +0000

    update

--- 02_Basics/03_objects.js
+++ 02_Basics/03_objects.js
@@ -1,4 +1,17 @@
 // literal
 //constructor
-
+//Object.create - object created through this method is called constructor method,aur isi ke ander singleton banta hai..
 // singleton = object made from constructor is called singleton
+
+
+const myarray = ["a","b"]    // in arrays we can access arrays only by one method 
+console.log(myarray[1]);     // can assess element by writing element no. in [] brackets.
+
+
+// object literals
+const jsUser = {
+    name : "arya",
+
+}
+
+
