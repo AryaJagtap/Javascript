8fe90e2d520055a0450740de6f7ecd1971a32fc9
Arya Jagtap <156169733+AryaJagtap@users.noreply.github.com>
2024-03-20 14:18:49 +0000

    project 6 and 5 done

---
+++ 07_projects/05_keyboard_check/script.js
@@ -0,0 +1,26 @@
+const insert = document.getElementById('insert')
+
+window.addEventListener('keydown',  (e) => {
+    insert.innerHTML = `
+    <div class = 'color'>
+    
+    <table>
+  <tr>
+    <th>key</th>
+    <th>keycode</th>
+    <th>code</th>
+  </tr>
+  <tr>
+    <td>${e.key === ' ' ? 'space' :e.key}</td>
+    <td>${e.keycode}</td>
+    <td>${e.code}</td>
+  </tr>
+    </table>
+
+    </div>
+    `
+})
+
+console.log("hello");
+
+// keycode propert has been removed from the js , so it wont work
