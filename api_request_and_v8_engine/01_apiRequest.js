c1f92ee3d8e6af79875ff20e6aa2d23287f5f73f
Arya Jagtap <156169733+AryaJagtap@users.noreply.github.com>
2024-03-23 18:55:50 +0000

    added Api Request and Promises

---
+++ api_request_and_v8_engine/01_apiRequest.js
@@ -0,0 +1,58 @@
+/*
+ API is a talking language between two systems, front end and backend
+ 1.frontend and backend both can be our
+ 2.frontend can be ours backend can be of someone else
+ 3.backend can be ours frontend can be of someone else
+ 
+ may be your two systems want to communicate with each outher one is written in java and other in js, ek ruby dusra php ,etc etc
+ so in dono ke beech jo baat karne ka medium hota hai use API bolte hai
+
+ format - ek bada sa object uske ander bhot sari values
+ example:-
+ {
+  "login": "AryaJagtap",
+  "id": 156169733,
+  "node_id": "U_kgDOCU72BQ",
+  "avatar_url": "https://avatars.githubusercontent.com/u/156169733?v=4",
+  "gravatar_id": "",
+  "url": "https://api.github.com/users/AryaJagtap",
+  "html_url": "https://github.com/AryaJagtap",
+  "followers_url": "https://api.github.com/users/AryaJagtap/followers",
+  "following_url": "https://api.github.com/users/AryaJagtap/following{/other_user}",
+  "gists_url": "https://api.github.com/users/AryaJagtap/gists{/gist_id}",
+  "starred_url": "https://api.github.com/users/AryaJagtap/starred{/owner}{/repo}",
+  "subscriptions_url": "https://api.github.com/users/AryaJagtap/subscriptions",
+  "organizations_url": "https://api.github.com/users/AryaJagtap/orgs",
+  "repos_url": "https://api.github.com/users/AryaJagtap/repos",
+  "events_url": "https://api.github.com/users/AryaJagtap/events{/privacy}",
+  "received_events_url": "https://api.github.com/users/AryaJagtap/received_events",
+  "type": "User",
+  "site_admin": false,
+  "name": "Arya Jagtap",
+  "company": "Atharva College Of Engineering",
+  "blog": "https://atharvacoe.ac.in/",
+  "location": "Malad",
+  "email": null,
+  "hireable": null,
+  "bio": "I am Second Year Computer Engineer Student From Mumbai University.",
+  "twitter_username": "aryajagtap10",
+  "public_repos": 5,
+  "public_gists": 0,
+  "followers": 4,
+  "following": 6,
+  "created_at": "2024-01-10T06:59:52Z",
+  "updated_at": "2024-03-12T13:15:55Z"
+}
+
+ my github api link : https://api.github.com/users/AryaJagtap
+
+ randomapi.me : website for random API
+ jason formattor : used to format data to make it into json
+
+
+
+ fetch se pahele xmlhttpRequest use karte the
+ 
+
+
+ */
