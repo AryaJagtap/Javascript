aa030a5275138ab393ef30511652c78e2ac9ab31
Arya Jagtap <156169733+AryaJagtap@users.noreply.github.com>
2024-04-14 19:20:26 +0000

    classes done

--- 11_classes_and_oops/06_classes.js
+++ 11_classes_and_oops/06_classes.js
@@ -1,2 +1,47 @@
 // all after ES6
 
+class User1 {
+    // below one is constructor
+    constructor (username, email, password){
+        this.username = username
+        this.email = email
+        this.password = password
+    }
+    
+    // below ones are methods
+    encryptPassword(){
+        return `${this.password}abc`
+    }
+
+    changeUsername(){
+        return `${this.username.toUpperCase()}`
+    }
+}
+
+const chai = new User1 ("chai", "chai@mail" ,"123")
+
+console.log(chai.encryptPassword());
+console.log(chai.changeUsername());
+
+/////////////////////////////////////////////////////////////////////
+
+// behind the scene without using class
+
+function User2 (username, email, password){
+    this.username = username
+    this.email = email
+    this.password = password
+}
+
+User2.prototype.encryptPassword = function (){
+    return `${this.password}abc`
+}
+
+User2.prototype.changeUsername = function (){
+    return `${this.username.toUpperCase()}`
+}
+
+const tea = new User1 ("tea", "chai@mail" ,"123")
+
+console.log(tea.encryptPassword());
+console.log(tea.changeUsername());
